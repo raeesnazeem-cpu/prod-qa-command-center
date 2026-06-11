46017361c13ffa5d0c548793eac151b94ae582b4
raeesnazeem <91611445+raeesnazeem@users.noreply.github.com>
2026-06-11 23:36:40 +0530

    ws realtime for supabase added

--- apps/api/src/lib/supabase.ts
+++ apps/api/src/lib/supabase.ts
@@ -1,19 +1,23 @@
-import { createClient } from '@supabase/supabase-js'
+import ws from "ws"
+import { createClient } from "@supabase/supabase-js"
 
 const supabaseUrl = process.env.SUPABASE_URL
 const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY
 
 if (!supabaseUrl) {
-  throw new Error('Missing environment variable: SUPABASE_URL')
+  throw new Error("Missing environment variable: SUPABASE_URL")
 }
 
 if (!supabaseServiceRoleKey) {
-  throw new Error('Missing environment variable: SUPABASE_SERVICE_ROLE_KEY')
+  throw new Error("Missing environment variable: SUPABASE_SERVICE_ROLE_KEY")
 }
 
 export const supabase = createClient(supabaseUrl, supabaseServiceRoleKey, {
   auth: {
     autoRefreshToken: false,
     persistSession: false,
+  },
+  realtime: {
+    transport: ws as any,
   },
 })
